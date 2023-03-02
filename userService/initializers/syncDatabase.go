cf35cf251c063115b943f8a1b886c8eaa676fb76
Shayan <shayanshabanzadeh0@gmail.com>
2023-03-02 13:04:25 +0330

    refactor: add all entities to syncDatabase

--- userService/initializers/syncDatabase.go
+++ userService/initializers/syncDatabase.go
@@ -4,4 +4,10 @@ import "userService/entities"
 
 func SyncDatabase() {
 	DB.AutoMigrate(&entities.User{})
+	DB.AutoMigrate(&entities.CV{})
+	DB.AutoMigrate(&entities.Education{})
+	DB.AutoMigrate(&entities.PersonalInfo{})
+	DB.AutoMigrate(&entities.Experience{})
+	DB.AutoMigrate(&entities.Skill{})
+	DB.AutoMigrate(&entities.SocialMedia{})
 }
