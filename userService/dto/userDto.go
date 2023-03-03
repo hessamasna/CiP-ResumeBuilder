5be1938f275cda173549d48b842196f70b0750ac
Shayan <shayanshabanzadeh0@gmail.com>
2023-03-03 17:09:12 +0330

    refactor: change Password_hash to password

--- userService/dto/userDto.go
+++ userService/dto/userDto.go
@@ -3,7 +3,7 @@ package dto
 type UserDto struct {
 	User_id       int    `json:"user_id"`
 	Email         string `json:"email"`
-	Password_hash string `json:"password_hash"`
+	Password_hash string `json:"password"`
 	Phone_number  string `json:"phone_number"`
 	Gender        string `json:"gender"`
 	First_name    string `json:"first_name"`
