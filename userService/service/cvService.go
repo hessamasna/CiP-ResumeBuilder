8cdf11af26dd8f332f8421325c48498eeaa19d9c
Shayan <shayanshabanzadeh0@gmail.com>
2023-03-03 18:22:54 +0330

    refactor: add get_cvs_by_user_id

--- userService/service/cvService.go
+++ userService/service/cvService.go
@@ -8,29 +8,19 @@ import (
 )
 
 func AddCv(cvDto dto.CVDto) (*dto.CVDto, *errors.Base_error) {
-	// if cvDto.ID != 0 {
-	// 	cv, error := repository.FindCVById(cvDto.ID)
-	// 	if error != nil {
-	// 		return nil, error
-	// 	}
-	// 	if cv != nil {
-	// 		error_message := fmt.Sprintf("Cv with ID : %v already exists", cv.ID)
-	// 		return nil, errors.New_entity_already_exist_error(error_message, nil).Error
-	// 	}
-	// }
 	cvDto.ID = 0
 
-	if cvDto.User_id == 0  {
+	if cvDto.UserID == 0 {
 		error_message := "User ID must be specified"
-		return nil , errors.New_Invalid_request_error(error_message , nil).Error
+		return nil, errors.New_Invalid_request_error(error_message, nil).Error
 	}
-	userDto , error := repository.FindUserById(int(cvDto.User_id))
+	userDto, error := repository.FindUserById(int(cvDto.UserID))
 	if error != nil {
 		return nil, error
 	}
 	if userDto == nil {
-		error_message := fmt.Sprintf("User with ID : %v  does not exist."  , cvDto.User_id)
-		return nil , errors.New_entity_does_not_exist_error(error_message , nil).Error
+		error_message := fmt.Sprintf("User with ID : %v  does not exist.", cvDto.UserID)
+		return nil, errors.New_entity_does_not_exist_error(error_message, nil).Error
 	}
 	_, err := repository.AddCv(cvDto)
 	if err != nil {
@@ -39,4 +29,20 @@ func AddCv(cvDto dto.CVDto) (*dto.CVDto, *errors.Base_error) {
 	}
 
 	return &cvDto, nil
+}
+
+func Get_cvs_by_user_id(id int) ([]dto.CVDto, *errors.Base_error) {
+	if id == 0 {
+		error_message := "User ID must be specified"
+		return nil, errors.New_Invalid_request_error(error_message, nil).Error
+	}
+	userDto, error := repository.FindUserById(id)
+	if error != nil {
+		return nil, error
+	}
+	if userDto == nil {
+		error_message := fmt.Sprintf("User with ID : %v  does not exist.", id)
+		return nil, errors.New_entity_does_not_exist_error(error_message, nil).Error
+	}
+	return repository.GetCVsByUserId(id)
 }
